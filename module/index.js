424669e9cc0b5d31b55e28425303e9a37cbc5a3b
Pratigya <pratigyadhakal4444@gmail.com>
2023-11-28 11:23:08 +0545

    module import and export

---
+++ module/index.js
@@ -0,0 +1,12 @@
+// const operation = require("./operation");
+// console.log(operation.add(15, 15));
+// console.log(operation.sub(25, 5));
+
+const { add, sub , mul, div , name} = require("./operation");
+
+// console.log(operation);
+console.log(add(5, 15));
+console.log(sub(5, 15));
+console.log(mul(5, 15));
+console.log(div(35, 15));
+console.log(name);
