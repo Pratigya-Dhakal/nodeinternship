7c7bb9b5c8b975e82e0271899ff246cd6ba7d0c7
Pratigya <pratigyadhakal4444@gmail.com>
2023-11-29 14:43:46 +0545

    routing in node js handled

--- 2ndDay/webServerExample.js
+++ 2ndDay/webServerExample.js
@@ -4,11 +4,29 @@ const hostname = '127.0.0.1';
 const port = 4000;
 
 const server = http.createServer((request, response) => {
-    response.statusCode = 200;
+    // console.log(request.url);
+    if(request.url == "/"){
+        response.end('2nd day task to send client request to node js application.\n');
+    }
+    else if(request.url == "/about") {
+        response.end('Form about 2nd day task to send client request to node js application.\n');
+
+    }
+    else if(request.url == "/contact") {
+        response.end('contact us Form about 2nd day task to send client request to node js application.\n');
+
+    } else if(request.url == "/home") {
+        response.end(' Home page hello\n');
+    }
+    else{
+        response.end('<h1>404 Page do not exist</h1>\n');
+    response.statusCode = 404;
     response.setHeader( 'Content-Type', 'text/plain' );
-    response.end('2nd day task to send client request to node js application.\n');
+    }
+    // response.statusCode = 200;
+    // response.setHeader( 'Content-Type', 'text/plain' );
 });
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
-});
+});  
