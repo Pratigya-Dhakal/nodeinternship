897a3cf2f7f36e28e018b49c4fc8778903ca8b90
Pratigya <pratigyadhakal4444@gmail.com>
2023-12-21 14:16:26 +0545

    authenticate token

--- project/src/middleware/authenticateToken.js
+++ project/src/middleware/authenticateToken.js
@@ -1,18 +1,29 @@
-// src/middleware/authenticateToken.js
 const { verifyToken } = require('../utils/jwt');
-
 function authenticateToken(req, res, next) {
-const token = req.header('Authorization');
+    const authHeader  = req.header('Authorization');
+    console.log(authHeader)
+    const token = authHeader.split(" ")[1]
+    console.log(token)
+    
+    console.log('Received token:', token); 
 
-if (!token) return res.status(401).json({ error: 'Unauthorized' });
+    if (!token) {
+        console.log('Token missing');
+        return res.status(401).json({ error: 'Unauthorized: Token missing' });
+    }
 
-const decoded = verifyToken(token);
+    const decoded = verifyToken(token);
 
-if (!decoded) return res.status(403).json({ error: 'Forbidden' });
+    console.log('Decoded token:', decoded);
+    if (!decoded) {
+        console.log('Invalid token');
+        return res.status(403).json({ error: 'Forbidden: Invalid token' });
+    }
 
-req.user = decoded;
+    req.user = decoded;
 
-next();
+    next();
 }
+
 
 module.exports = authenticateToken;
