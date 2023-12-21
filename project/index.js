7ecc62f435f119ad93b68a109844b3a0d52a881f
Pratigya <pratigyadhakal4444@gmail.com>
2023-12-21 14:16:57 +0545

    main

--- project/index.js
+++ project/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const registrationRouter = require('./src/routes/registrationRouter');
 const loginRouter = require('./src/routes/loginRouter');
+const authenticateToken = require('./src/middleware/authenticateToken');
+
 
 const app = express();
 const PORT = 3003;
@@ -19,6 +21,9 @@ res.render('pages/index');
 
 app.use(registrationRouter);
 app.use( loginRouter); 
+app.get('/profile',authenticateToken, (req, res) => {
+    res.send('This is the profile page');
+});
 
 app.listen(PORT, () => {
 console.log(`Server is running on port ${PORT}`);
